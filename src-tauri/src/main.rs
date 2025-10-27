425c974abcfa0c3f06f8001754841b587756a7a5
Ktclat <124330311+Ktclat@users.noreply.github.com>
2025-10-28 04:54:54 +0800

    Set WebView2 browser executable folder path
    Signed-off-by: Ktclat <124330311+Ktclat@users.noreply.github.com>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,7 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
-
+std::env::set_var("WEBVIEW2_BROWSER_EXECUTABLE_FOLDER", "./WebView2Runtime");
 fn main() {
     app_lib::run()
 }
