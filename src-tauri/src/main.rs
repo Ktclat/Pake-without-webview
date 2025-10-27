3b33c09d4f31117f1315731a9ce1ac330c0b57c3
Ktclat <124330311+Ktclat@users.noreply.github.com>
2025-10-28 05:43:23 +0800

    更新 main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,10 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
-std::env::set_var("WEBVIEW2_BROWSER_EXECUTABLE_FOLDER", "./WebView2Runtime");
+
 fn main() {
-    app_lib::run()
+    // 指向本地打包好的 WebView2
+    std::env::set_var("WEBVIEW2_BROWSER_EXECUTABLE_FOLDER", "./WebView2Runtime");
+    
+    app_lib::run();
 }
